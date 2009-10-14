fee737a0576b0070136fdf6112fe15168e30cf17
Wilfred Hughes <wilfred@alto.chu.cam.ac.uk>
2009-10-14 23:38:23 +0100

    added timetable to proposal

--- doc/makepdf.sh
+++ doc/makepdf.sh
@@ -2,12 +2,13 @@
 
 # a rather noisy script to run latex
 
-echo -e 'Writing proposal pdf'
+echo -e '\e[0;31mWriting proposal pdf \e[m'
 pdflatex proposal.tex
 
+# to include a latex file in another we cannot have the preamble
 # this could be smarter, but assume the size of the header is constant
-echo -e 'Generating proposal include'
+echo -e '\e[0;31mGenerating proposal include \e[m'
 tail --lines=+10 proposal.tex | head --lines=-1 >proposal_include.tex
 
-echo -e 'Writing dissertation pdf'
+echo -e '\e[0;31mWriting dissertation pdf \e[m'
 pdflatex dissertation.tex
