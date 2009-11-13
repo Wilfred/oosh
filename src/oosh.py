ca61f052fef8d10fcbda67d74799157dc4743459
Wilfred Hughes <wilfred@alto.chu.cam.ac.uk>
2009-11-13 16:30:33 +0000

    moving invokable programs to separate file

--- src/oosh.py
+++ src/oosh.py
@@ -8,13 +8,13 @@ from cmd import Cmd
 import readline
 # regular expressions
 import re
-# list files for ls command
-import os
-# to enable us to convert user ids to strings
-import pwd
 
 # Cmd gives us: ? (sugar for command 'help') ! (sugar for command 'shell')
 # do_whatever() to implement builtins, help_whatever() to document
+
+# current location of oosh programs:
+# todo: create /usr/bin equivalent
+import programs
 
 class Oosh(Cmd):
     savedpipes = {}
@@ -35,7 +35,6 @@ class Oosh(Cmd):
             # save our piped data
             self.savedpipes[lasttoken[1:]] = pipeddata
 
-
     def pipedcmd(self, line, pipein):
         cmd, arg, line = self.parseline(line)
         if not line:
@@ -45,7 +44,7 @@ class Oosh(Cmd):
         self.lastcmd = line
         try:
             # find a command with this name
-            func = getattr(self, 'do_' + cmd)
+            func = getattr(programs, 'do_' + cmd)
         except AttributeError:
             return self.default(line, pipein)
         return func(arg, pipein)
@@ -55,80 +54,6 @@ class Oosh(Cmd):
         args = line.split(" ")
         self.stdout.write('oosh doesn\'t know the command \'%s\'. Try typing \'help\' to list commands.\n'%args[0])
         return []
-
-    # basic builtins
-    def do_help(self, line, pipein):
-        super(Oosh, self).do_help(line)
-        return []
-    def do_exit(self, line, pipein):
-        exit()
-        return [] # by convention
-    def help_exit(self):
-        self.print_topics("exit", ["Exit oosh"], 15, 80)
-
-    def do_shell(self, line, pipein):
-        print("shell escape coming soon")
-        return []
-    def help_shell(self):
-        self.print_topics("shell [shell command]", 
-                          ["Execute a command in bash"], 15, 80)
-
-    def do_echo(self, line, pipein):
-        # echo text data inputted by user, drop pipe in
-        return [Droplet(line)]
-    def help_echo(self):
-        self.print_topics("echo [item1, item2, ...]", 
-                          ["Create a list of items for the pipe"], 15, 80)
-
-    def do_select(self, line, pipein):
-        # may be better to use similar arguments to rename
-        args = line.split(" ") # command name is not passed
-        for droplet in pipein:
-            for entry in droplet.entries:
-                if entry not in args:
-                    del droplet.entries[entry]
-        return pipein
-    def help_select(self):
-        self.print_topics("select [column1 column2 ...]", 
-                          ["Only returns droplets with the column names given (assumes column names are single word)"], 15, 80)
-
-    def do_rename(self, line, pipein):
-        args = re.findall('".*?"', line, flags=re.DOTALL)
-        # strip "
-        changes = [s[1:][:-1] for s in args]
-        if len(changes) % 2 != 0:
-            print('rename requires an even number of arguments')
-            raise PipeError
-        else:
-            for droplet in pipein:
-                for entry in list(droplet.entries):
-                    # iterate over replacements
-                    for i in range(0,len(changes),2):
-                        if changes[i] == entry:
-                            value = droplet.entries.pop(entry)
-                            droplet.entries[changes[i+1]] = value
-            return pipein
-
-    def do_project(self, line, pipein):
-        # this duplicates some of select's functionality
-        args = re.findall('".*?"', line, flags=re.DOTALL)
-        # strip "
-        projected = [s[1:][:-1] for s in args]
-        for droplet in pipein:
-            for entry in list(droplet.entries):
-                if entry not in projected:
-                    del droplet.entries[entry]
-        return pipein
-
-    def do_ls(self, line, pipein):
-        pipeout = []
-        for filename in os.listdir("."):
-            user = pwd.getpwuid(os.stat(filename).st_uid).pw_name
-            size = os.stat(filename).st_size
-            fileinfo = [['Filename', filename], ['Owner', user],
-                        ['Size', size]]
-            pipeout.append(Droplet(fileinfo))
-        return pipeout
 
 # an object stream is made of droplets
 class Droplet:
