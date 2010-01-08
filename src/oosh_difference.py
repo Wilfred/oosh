b880dd8d6bc985cb3637787bbb44f35b96a6772c
Wilfred Hughes <wilfred@alto.chu.cam.ac.uk>
2010-01-08 16:31:37 +0000

    adding some commands that demonstrate multipipes

---
+++ src/oosh_difference.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+args = sys.argv[1:-2] # 1st arg to n-1th arg
+second_pipe_fd = int(sys.argv[-1])
+
+first_pipe_in = sys.stdin.read().splitlines()
+second_pipe_in = os.fdopen(second_pipe_fd).read().splitlines()
+
+first_lines = [eval(v) for v in first_pipe_in[:-1]]
+second_lines = [eval(v) for v in second_pipe_in[:-1]]
+
+difference = []
+for line in first_lines:
+    if not line in second_lines:
+        difference.append(line)
+
+for dic in difference:
+    sys.stdout.write(dic.__repr__())
+    sys.stdout.write('\n')
---
+++ src/oosh_union.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+args = sys.argv[1:-2] # 1st arg to n-1th arg
+second_pipe_fd = int(sys.argv[-1])
+
+first_pipe_in = sys.stdin.read().splitlines()
+second_pipe_in = os.fdopen(second_pipe_fd).read().splitlines()
+
+first_lines = [eval(v) for v in first_pipe_in[:-1]]
+second_lines = [eval(v) for v in second_pipe_in[:-1]]
+
+unioned = first_lines + second_lines
+
+for dic in unioned:
+    sys.stdout.write(dic.__repr__())
+    sys.stdout.write('\n')
