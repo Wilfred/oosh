075c724858f032197233f99a91672e89a698cea9
Wilfred Hughes <wilfred@alto.chu.cam.ac.uk>
2010-01-12 15:26:11 +0000

    removed some old unused code

--- src/oosh.py
+++ src/oosh.py
@@ -128,60 +128,6 @@ class Oosh(Cmd):
         else:
             raise InvalidTreeException
 
-    def pipedcmd(self, line, pipein):
-        cmd, arg, line = self.parseline(line)
-        if not line:
-            return pipein # do nothing with empty line
-        if cmd is None or cmd == '':
-            return self.default(line, pipein)
-        self.lastcmd = line
-        try:
-            # find a command with this name
-            func = getattr(programs, 'do_' + cmd)
-        except AttributeError:
-            return self.default(line, pipein)
-        return func(arg, pipein)
-
-    def multipipedcmd(self, line, pipes):
-        # do multi pipe to start
-        try:
-            multipipe = [copy.deepcopy(self.saved_pipes[name]) for name in pipes]
-            cmd, arg, line = self.parseline(line)
-            func = getattr(programs, 'do_multi_' + cmd)
-        except AttributeError:
-            return self.default(line, pipeddata)
-        except KeyError:
-            print("You have not saved a pipe of that name. Todo: state name")
-            return []
-        return func(arg, multipipe)
-     
-    def startswithnamedpipe(self, line):
-        beginning = line.split(" ")[0]
-        # pipe syntax is |1+2+3 to combine saved pipes
-        if re.match("\|[0-9]+(\+[0-9]+)*", beginning) is None:
-            return False
-        else:
-            return True
-
-    def endswithnamedpipe(self, line):
-        end = line.split(" ")[-1]
-        if re.match("\|[0-9]+", end) is None:
-            return False
-        else:
-            return True
-
-    def getpipenames(self, pipenames):
-        pipes = pipenames.split("+")
-        # first has leading |
-        pipes[0] = pipes[0][1:]
-        return pipes
-
-    # define error message with unknown command
-    def default(self, line, pipein):
-        args = line.split(" ")
-        self.stdout.write('oosh doesn\'t know the command \'%s\'.\n'%args[0])
-        return []
-
 # an object stream is made of droplets
 class Droplet:
     def __init__(self, value):
