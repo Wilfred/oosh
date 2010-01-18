2ab5a9ca8da28006b76a98fc1d50bfa7caf72cef
Wilfred Hughes <wilfred@alto.chu.cam.ac.uk>
2010-01-18 21:58:19 +0000

    added support in server for several commands in pipeline without sending traffic

--- src/ooshserver.py
+++ src/ooshserver.py
@@ -3,10 +3,11 @@ import hashlib
 import oosh
 from oosh import OoshError
 import re
+import subprocess
 
 user_logins = {'wilfred':('6f5902ac23', 'f21627d3faf150060b0b2cb3902c05ef2fdda3ae')}
 connected_machines = []
-stored_data = []
+stored_data = b''
 
 def checklogin(username, password):
     (salt, hash) = user_logins[username]
@@ -23,14 +24,24 @@ class OoshRequestHandler(socketserver.BaseRequestHandler):
     # it is instantiated once per connection to the server
 
     def handle(self):
+        global user_logins
+        global connected_machines
+        global stored_data
+
         # self.request is the TCP socket connected to the client
         raw_request = self.request.recv(1024).strip()
         request = raw_request.decode().split()
+        if len(request) < 1:
+            return
+
         address = self.client_address[0]
         print("request is",request)
 
         if request[0] == 'connect':
             # authenticate if not already
+            if len(request) < 3:
+                self.request.send(b'Please specify a username and password')
+                return
             username = request[1]
             password = request[2]
             if address not in connected_machines:
@@ -39,30 +50,30 @@ class OoshRequestHandler(socketserver.BaseRequestHandler):
                     self.request.send(b'Connected')
                 else:
                     self.request.send(b'Invalid password specified')
+
         elif self.client_address[0] in connected_machines:
             # execute requested command, or disconnect if asked
             if request[0] == 'disconnect':
                 connected_machines.remove(address)
                 self.request.send(b'Disconnected')
             elif request[0] == 'send':
-                data_to_send = ''
-                for line in stored_data:
-                    data_to_send += line.__repr__() + '\n'
-                self.request.send(data_to_send.encode())
-                stored_data = []
+                self.request.send(stored_data)
+                stored_data = b''
             elif request[0] == 'command':
                 try:
-                    self.shell_command(request[1:])
+                    stored_data = self.shell_command(request[1:])
                 except OoshError as error:
                     self.request.send(error.message.encode())
-                    stored_data = []
+                    stored_data = b''
             else:
                 error = "Invalid request:" + str(request)
                 self.request.send(error.encode())
         else:
             self.request.send(b'You need to login first')
 
-    def shell_command(self, command_string):
+    def shell_command(self, command):
+        command_name = command[0]
+
         if not re.match('oosh_.*', command_name) is None:
             if command_name == 'oosh_graph':
                 # graph uses pycairo, which is not py3k compatible
@@ -70,8 +81,18 @@ class OoshRequestHandler(socketserver.BaseRequestHandler):
             else:
                 command = ['python3'] + [command_name + '.py'] + command[1:]
         try:
-            process = subprocess.Popen(command, stdin=stdin, stdout=subprocess.PIPE)
-            return process
+            if stored_data == b'':
+                # no previous command in pipeline
+                process = subprocess.Popen(command, stdout=subprocess.PIPE)
+            else:
+                previous = oosh.Oosh.pipe_from_data(None, stored_data)
+                process = subprocess.Popen(command, stdin=previous,
+                                           stdout=subprocess.PIPE)
+            process.wait()
+            return_code = process.returncode
+            if return_code != 0:
+                raise OSError
+            return process.stdout.read()
         except OSError:
             raise OoshError("No such command: " + command_name)
 
