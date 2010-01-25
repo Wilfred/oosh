e518842a9b3bcfa3247e39e0b5d6e17e54d30f8c
Wilfred Hughes <wilfred@alto.chu.cam.ac.uk>
2010-01-25 17:51:34 +0000

    fixing bugs in oosh_sort that did string sort on numbers

--- src/oosh_project.py
+++ src/oosh_project.py
@@ -2,7 +2,8 @@ import sys
 
 args = sys.argv[1:]
 if len(args) < 1:
-    sys.exit(0) # done
+    print("Usage: oosh_project column1 column2 ...")
+    sys.exit(1)
 pipein = sys.stdin.read().splitlines()
 lines = [eval(v) for v in pipein[:-1]]
 for dic in lines:
--- src/oosh_sort.py
+++ src/oosh_sort.py
@@ -3,14 +3,31 @@ import operator
 from oosh import OoshError
 
 args = sys.argv[1:]
+if len(args) < 1:
+    print("Usage: oosh_sort columnname")
+    sys.exit(1)
+
 sort_on = args[0]
 pipein = sys.stdin.read().splitlines()
 lines = [eval(v) for v in pipein[:-1]]
-try:
-    lines.sort(key=operator.itemgetter(sort_on))
-    for dic in lines:
-        sys.stdout.write(dic.__repr__())
-        sys.stdout.write('\n')
-except KeyError:
+# check we are sorting on a valid column
+if len(lines) > 0 and sort_on not in lines[0].keys():
     print(sort_on,"is not a valid column name")
-    sys.exit(1)
+    sys.exit(1)  
+
+# treat data numerically if possible, it comes in as a string
+is_numeric = True
+for line in lines:
+    try:
+        line[sort_on] = float(line[sort_on])
+    except ValueError:
+        is_numeric = False
+        break
+# if we have mixed data, we may have converted to float, so revert
+if not is_numeric:
+    for line in lines:
+        line[sort_on] = str(line[sort_on])
+
+lines.sort(key=operator.itemgetter(sort_on))
+for dic in lines:
+    sys.stdout.write(dic.__repr__() + '\n')
